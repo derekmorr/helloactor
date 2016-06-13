f26a48d3c3f21127b449d44a002513292eef37e3
Derek Morr <derek.morr.test@gmail.com>
2016-06-13 09:00:23 -0400

    simplify code a bit

--- src/main/scala/org/derekmorr/Runner.scala
+++ src/main/scala/org/derekmorr/Runner.scala
@@ -8,22 +8,16 @@ import org.derekmorr.HelloActor.SaySomething
  * Driver for HelloActor
  */
 object Runner extends App {
-
-  val msg1 = SaySomething("This is message 1.")
-  val msg2 = SaySomething("This is message 2.")
-  val msg3 = SaySomething("This is message 3.")
-  val msg4 = SaySomething("This is message 4.")
-
   println("starting up actor system")
   val actorSystem = ActorSystem("CrashySystem")
 
   val helloActor = actorSystem.actorOf(Props[HelloActor], "helloactor")
 
   // send our messages
-  helloActor ! msg1
-  helloActor ! msg2
-  helloActor ! msg3
-  helloActor ! msg4
+  helloActor ! SaySomething("This is message 1.")
+  helloActor ! SaySomething("This is message 2.")
+  helloActor ! SaySomething("This is message 3.")
+  helloActor ! SaySomething("This is message 4.")
 
   Thread.sleep(2000)
   println("shutting down actor system")
